bb4428438395d879b36bacee64b1528e7eb664f6
Aymankhalif <harithkhalif90@gmail.com>
2023-03-17 09:50:26 +0300

    done with the assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(record) {
+    const winRecord = record.find(game => game.result === "W");
+    if (winRecord) {
+      return winRecord.year;
+    } else {
+      return undefined;
+    }
+  }
+  
